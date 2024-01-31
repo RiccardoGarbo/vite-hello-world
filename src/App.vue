1349a7d77411dcb88ab24053d66fd88b8098389f
RiccardoGarbo <Riccardo.garbo1994@gmail.com>
2024-01-31 18:36:05 +0100

    add new component with jumbotron

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <script >
 import AppTitle from './components/AppTitle.vue'
+import AppHero from './components/AppHero.vue'
 export default {
-  components: { AppTitle }
+  components: { AppTitle, AppHero }
 }
 </script>
 
 <template>
-  <AppTitle />
+  <header>
+    <AppTitle />
+  </header>
+  <section id="jumbotron">
+    <AppHero />
+  </section>
 </template>
 
 <style scoped></style>
